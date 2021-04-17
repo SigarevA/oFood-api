cc34653b77c13775d56df689458ad424440a28b9
Sigarev Aleksei <afakusima@mail.ru>
2021-04-17 14:51:34 +0300

    init api

---
+++ src/main/java/ru/vsu/ofoodApi/oFoodApi/OFoodApiApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.ofoodApi.oFoodApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OFoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vsu/ofoodApi/oFoodApi/OFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.ofoodApi.oFoodApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OFoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
